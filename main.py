cba7a5f9ad52410ede2ae8cecf3e1dc883816142
Vladislav Slobodchikov <vslobodchikov@vvo.local>
2026-06-04 21:41:27 +1000

    Add division exceptions

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+first_num = input("Enter first number: ")
+second_num = input("Enter second number: ")
 
+try:
+    division_result = float(first_num) / float(second_num)
+    print(division_result)
+except ZeroDivisionError:
+    print("Division by zero")
+except ValueError:
+    print("Not int value")
+finally:
+    print("End.")
